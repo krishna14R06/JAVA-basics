08f49a98ff4e7d34c1437fdf9d678560599703a5
krishna14R06 <krishnarathore1406@gmail.com>
2026-04-21 00:14:22 +0530

    Mastered 2D matrix logic and nested loops

--- patternsD4.java
+++ patternsD4.java
@@ -1,41 +1,52 @@
-// import java.util.*;
+import java.util.*;
 
 public class patternsD4 {
     public static void main(String args[]) {
-        // Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the box (Rows):");
-        // int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
         
-        // int columns = n + 1; 
-
-        // for (int i = 1; i <= n; i++) {
-            
-        //     for (int j = 1; j <= columns; j++) {
-                
-        //         if (i == 1 || i == n || j == 1 || j == columns) {
-        //             System.out.print("*");
-        //         } else {
-        //             System.out.print(" "); 
-        //         }
-        //     }
-        //     System.out.println();
-        // }
-        int n = 4;
-        // for(int a = 1; n>=a; a++){
-        //     for(int b =1;(n-a+1)>=b; b++){
-        //         System.out.print("*");
-        //     }System.out.println();
-        // }
-
-        for(int a = 1; n>=a; a++){
-            for(int b = 1; b<=(n-a); b++){
-                System.out.print(" ");
+        System.out.print("Enter the size (n) for the patterns: ");
+        int n = sc.nextInt();
+        
+        System.out.println("\n--- Pattern 1: Hollow Rectangle ---");
+        int columns = n + 1; 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if (i == 1 || i == n || j == 1 || j == columns) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" "); 
+                }
             }
-            for(int c = 1; c<=a; c++){
-                System.out.print("*");
-            }System.out.println();
+            System.out.println();
         }
 
-        // sc.close();
+        System.out.println("\n--- Pattern 2: Inverted Half-Pyramid ---");
+        for(int a = 1; n >= a; a++){
+            for(int b = 1; (n - a + 1) >= b; b++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n--- Pattern 3: 180-Degree Leaning Pyramid ---");
+        for(int a = 1; n >= a; a++){
+            for(int b = 1; b <= (n - a); b++){
+                System.out.print(" ");
+            }
+            for(int c = 1; c <= a; c++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n--- Pattern 4: Number Half-Pyramid ---");
+        for(int a = 1; n >= a; a++){
+            for(int b = 1; a >= b; b++){
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+        
+        sc.close();
     }
 }
