47dc44a7a7179b0f41bd81a6dba1d2977d58c72b
krishna14R06 <krishnarathore1406@gmail.com>
2026-05-12 20:49:23 +0530

    Array

--- Arrays.java
+++ Arrays.java
@@ -1,57 +1,85 @@
-import java.util.*;
+public class Arrays {
 
-
-public static int LinSearch(int num[], int search){ //Q1 linear Search of an array
-    for(int i = 0; i < num.length; i++){
-        if(search == num[i]){
-            return i;
+    // --- TOOL 1: Linear Search ---
+    public static int linearSearch(int num[], int search) {
+        for(int i = 0; i < num.length; i++) {
+            if(search == num[i]) {
+                return i;
+            }
         }
-    }return -1;
-}
-
-public static int LargestinArr(int num[], int largest){ //Q2 finding largest value in the array
-    for(int i = 0; i < num.length; i++){
-        if(largest < num[i]){
-            largest = num[i];
-        }
-    }return largest;
-}
-
-public static int Binarysearch(int sortedarr[], int target){
-    int start = 0;
-    int end = sortedarr.length - 1;
-    while(start <= end){
-        int mid = (start + end) / 2;
-        if(target == sortedarr[mid]){
-            return mid;
-        }else if(target < sortedarr[mid]){
-            end = mid - 1;
-        }else{
-            start = mid + 1;
-        }
-    }
-    return -1;
-}
-public static void main(String args[]){
-    int num[] = {2, 5, 1, 8, 9, 4};
-    int sortedarr[] = {3, 6, 9, 12, 15, 18};
-    int search2 = 3;
-    int search = 4;
-    int largest = Integer.MIN_VALUE;
-
-    int index = LinSearch(num, search);
-    int largestvalue = LargestinArr(num, largest);
-    System.out.println("Largest value in your Array is:" + largestvalue);
-    if(index != -1 ){
-        System.out.println("Your num is at index " + index);
-    }else{
-        System.out.println("Your num not found");
+        return -1;
     }
 
-    int index2 = Binarysearch(sortedarr, search2);
-    if(index2 != -1){
-        System.out.println("Binary search found at index " + index2);
-    }else{
-        System.out.println("Binary search not found");
+    // --- TOOL 2: Find Largest Value --
+    public static int largestInArr(int num[]) {
+        int largest = Integer.MIN_VALUE; 
+        for(int i = 0; i < num.length; i++) {
+            if(largest < num[i]) {
+                largest = num[i];
+            }
+        }
+        return largest;
+    }
+
+    // --- TOOL 3: Binary Search ---
+    public static int binarySearch(int sortedarr[], int search2) {
+        int start = 0;
+        int end = sortedarr.length - 1; 
+        
+        while (start <= end) {          
+            int mid = (start + end) / 2;
+            
+            if(search2 == sortedarr[mid]) {
+                return mid;
+            } else if(search2 < sortedarr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            } 
+        }
+        return -1;
+    }
+
+    public static void reverse(int reverseArr[]){
+        int start = 0;
+        int end = reverseArr.length - 1;
+        while(start<end){
+            int temp = reverseArr[start];
+            reverseArr[start] = reverseArr[end];
+            reverseArr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String args[]) {
+        // Data Vaults
+        int num[] = {2, 5, 1, 8, 9, 4};
+        int sortedarr[] = {3, 6, 9, 12, 15, 18};
+        int reverseArr[] = {4,2,7,3,0,5};
+        
+        // Search Targets
+        int search = 4;
+        int search2 = 3;
+
+        // Executing Tools
+        int linearindex = linearSearch(num, search);
+        System.out.println("For your linear search value index is:" + linearindex);
+
+        int largestvalue = largestInArr(num);
+        System.out.println("Largest value in your Array is: " + largestvalue);
+        
+        int binindex = binarySearch(sortedarr, search2);
+        if(binindex != -1) {
+            System.out.println("Your num is at index " + binindex);
+        } else {
+            System.out.println("Your num not found");
+        }
+
+        reverse(reverseArr);
+        for(int i=0; i<reverseArr.length-1; i++){
+        System.out.print(reverseArr[i] + " ");
+        }
+        
     }
 }
